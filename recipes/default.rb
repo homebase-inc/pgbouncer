536d71b3aaf350c837fb348ffbb7b4ef43e4d457
Konstantin Gredeskoul <kigster@gmail.com>
2018-02-06 17:13:11 -0800

    Fix pgbouncer to install on ubuntu/systemd

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,2 @@
-
-include_recipe "pgbouncer::install_from_#{node[:pgbouncer][:install_method]}"
+include_recipe "pgbouncer::install_from_#{node['pgbouncer']['install_method']}"
 include_recipe 'pgbouncer::configure'
--- recipes/install_from_source.rb
+++ recipes/install_from_source.rb
@@ -1,9 +1,8 @@
+remote_file = node['pgbouncer']['source']['url']
+local_file  = remote_file.gsub(%r{.*/}, '') # pgbouncer-1.5.4.tar.gz
+local_dir   = local_file.gsub(%r{\.tar\.gz}, '') # pgbouncer-1.5.4
 
-remote_file                = node.pgbouncer.source.url
-local_file                 = remote_file.gsub(%r{.*/}, '')        # pgbouncer-1.5.4.tar.gz
-local_dir                  = local_file.gsub(%r{\.tar\.gz}, '')   # pgbouncer-1.5.4
-
-node.default[:pgbouncer][:version] = local_dir.gsub(%r{.*-}, '')  # 1.5.4
+node.default['pgbounscer']['version'] = local_dir.gsub(%r{.*-}, '') # 1.5.4
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{local_file}" do
   source remote_file
@@ -19,41 +18,30 @@ execute 'extract tar ball into file_cache_path' do
 end
 
 case node['platform']
-when 'smartos'
-  package 'libevent'
+  when 'smartos'
+    package 'libevent'
 
-  execute 'build pgbouncer' do
-    command [
-      "cd #{Chef::Config[:file_cache_path]}/#{local_dir}",
-      "./configure --with-libevent=/opt/local --prefix=#{node.pgbouncer.source.install_dir}",
-      'make install'
-    ].join(' && ')
+    execute 'build pgbouncer' do
+      command [
+                "cd #{Chef::Config[:file_cache_path]}/#{local_dir}",
+                "./configure --with-libevent=/opt/local --prefix=#{node['pgbouncer']['source']['install_dir']}",
+                'make install'
+              ].join(' && ')
 
-    environment 'CFLAGS' => '-m64',
-                'CXXFLAGS' => '-m64',
-                'LDFLAGS' => '-Wl,-R/opt/local/lib -L/opt/local/lib -m64'
-    not_if { File.exist?("#{node.pgbouncer.source.install_dir}/bin/pgbouncer") }
-  end
+      environment 'CFLAGS'   => '-m64',
+                  'CXXFLAGS' => '-m64',
+                  'LDFLAGS'  => '-Wl,-R/opt/local/lib -L/opt/local/lib -m64'
+      not_if { File.exist?("#{node['pgbouncer']['source']['install_dir']}/bin/pgbouncer") }
+    end
+  else
+    package 'libevent-dev'
 
-  smf 'pgbouncer' do
-    start_command "#{node.pgbouncer.source.install_dir}/bin/pgbouncer -d -u #{node.pgbouncer.os_user} /etc/pgbouncer/pgbouncer.ini"
-    refresh_command ':kill -HUP'
-    stop_command ':kill'
-    start_timeout 30
-    stop_timeout 30
-    working_directory '/'
-
-    ## TODO this can be removed once all instances compiled without LDFLAGS are gone
-    environment 'LD_LIBRARY_PATH' => '/opt/local/lib'
-  end
-
-else
-  execute 'build pgbouncer' do
-    command [
-      "cd #{Chef::Config[:file_cache_path]}/#{local_dir}",
-      "./configure --prefix=#{node.pgbouncer.source.install_dir}",
-      'make install'
-    ].join(' && ')
-    not_if { File.exist?("#{node.pgbouncer.source.install_dir}/bin/pgbouncer") }
-  end
+    execute 'build pgbouncer' do
+      command [
+                "cd #{Chef::Config[:file_cache_path]}/#{local_dir}",
+                "./configure --prefix=#{node['pgbouncer']['source']['install_dir']}",
+                'make install'
+              ].join(' && ')
+      not_if { File.exist?("#{node['pgbouncer']['source']['install_dir']}/bin/pgbouncer") }
+    end
 end
